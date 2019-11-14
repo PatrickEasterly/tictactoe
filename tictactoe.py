60855e85ec275d247774c2a585b565dc3acba0ed
Patrick <pateast678@gmail.com>
2019-11-14 10:01:31 -0500

    implements size exception

--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,21 @@ def print_board(board):
     for row in board:
         print(row)
 
+def is_valid_size(board):
+    is_valid = True
+    # 3 rows?
+    if len(board) != 3:
+        is_valid = False
+    for row in board:
+        if len(row) != 3:
+            is_valid = False
+    return is_valid
 def move(board, location, player):
+    #handle errors first
+    if not is_valid_size(board):
+        raise Exception('Game board size is not valid. ')
+
+
     # print(f'The player is {player}')
     row_number = location[0]
     col_number = location[1]
@@ -14,12 +28,13 @@ def move(board, location, player):
     return board
 
     pass
-
 game_board = [
-    [' ', ' ', ' '],
+    # [' ', ' ', ' '],
     [' ', ' ', ' '],
     [' ', ' ', ' ']
 ]
+# print(is_valid_size(game_board))
+
 player = 'O'
 loc = (0, 0)
 game_board = move(game_board, loc, player)
